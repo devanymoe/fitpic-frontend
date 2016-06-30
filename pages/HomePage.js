6c299df5bbe74ec2f0cf04fc630a4fe4ec779e88
Devany Moe <devanymoe@gmail.com>
2016-06-30 16:51:15 -0700

    Create settings menu

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -13,14 +13,51 @@ class HomePage extends Component {
     this.handlePress = this.handlePress.bind(this);
   }
 
-  handlePress() {
-    this.props.navigator.push({name: 'camera'});
+  handlePress(item) {
+    if (item === 'pictures') {
+      this.props.navigator.push({name: 'pictures'});
+    }
+    else if (item === 'measure') {
+      this.props.navigator.push({name: 'measure'});
+    }
+    else if (item === 'timeline') {
+      this.props.navigator.push({name: 'timeline'});
+    }
+    else if (item === 'progress') {
+      this.props.navigator.push({name: 'progress'});
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the homepage</Text>
+        <View style={styles.tileContainer}>
+          <View style={styles.boxRow}>
+            <TouchableHighlight onPress={this.handlePress.bind(this, 'pictures')}>
+              <View style={styles.box}>
+                <Text style={styles.boxText}>Pictures</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.handlePress.bind(this, 'measure')}>
+              <View style={styles.box}>
+                <Text style={styles.boxText}>Measurements</Text>
+              </View>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.boxRow}>
+            <TouchableHighlight onPress={this.handlePress.bind(this, 'timeline')} >
+              <View style={styles.box}>
+                <Text style={styles.boxText}>Timeline</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.handlePress.bind(this, 'progress')}>
+              <View style={styles.box}>
+                <Text style={styles.boxText}>Progress</Text>
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <Text>Put three most recent timeline items here</Text>
       </View>
     )
   }
@@ -29,9 +66,31 @@ class HomePage extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 10,
     alignItems: 'center',
     backgroundColor: '#fff'
+  },
+  box: {
+    margin: 10,
+    width: 150,
+    height: 150,
+    backgroundColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  boxRow: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  boxText: {
+    color: '#000',
+  },
+  tileContainer: {
+    marginBottom: 10,
+    paddingBottom: 20,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
   }
 });
 
--- pages/SettingsPage.js
+++ pages/SettingsPage.js
@@ -4,18 +4,52 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  TextInput,
+  Switch
 } from 'react-native';
+
 
 class SettingsPage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      metric: false
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the settings page</Text>
+        <View style={styles.inputContainer}>
+          <View style={styles.label}>
+            <Text style={styles.labelText}>
+              Email
+            </Text>
+          </View>
+          <TextInput style={styles.input} underlineColorAndroid='rgba(0,0,0,0)'/>
+        </View>
+        <View style={styles.inputContainer}>
+          <View style={styles.label}>
+            <Text style={styles.labelText}>
+              Username
+            </Text>
+          </View>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.inputContainer}>
+          <View style={styles.switchLabel}>
+            <Text style={styles.labelText}>
+              Metric (default U.S.)
+            </Text>
+          </View>
+          <Switch
+            onValueChange={(value) => this.setState({metric: value})}
+            value={this.state.metric} />
+        </View>
+
+
       </View>
     )
   }
@@ -24,9 +58,36 @@ class SettingsPage extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff'
+  },
+  input: {
+    flex: 1,
+    textAlign: 'right',
+    height: 30,
+    padding: 0,
+  },
+  inputContainer: {
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+    padding: 10,
+    paddingTop: 12,
+    paddingBottom: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  label: {
+    width: 100,
+    marginRight: 10
+  },
+  switchLabel: {
+    marginRight: 10,
+    flex: 1
+  },
+  labelText: {
+    fontSize: 16,
+    color: '#1c1c1c'
   }
 });
 
